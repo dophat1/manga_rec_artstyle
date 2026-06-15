a9fa6c6895bf275393cdafef928c0d46e2e6529b
dophat1 <dophat2403@gmail.com>
2026-06-15 15:56:21 +0200

    add embed.py

--- embed.py
+++ embed.py
@@ -2,5 +2,28 @@ from PIL import Image
 from torchvision import transforms
 import torch, timm , os, glob
 
+
+transform = transforms.Compose([transforms.Lambda(lambda x: x.convert("RGB")), transforms.Resize(224), transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]) 
+model = timm.create_model("caformer_s18.sail_in22k",pretrained=True, num_classes=0)
+    
+for param in model.parameters():
+    param.requires_grad = False
+
+
 def embedding_manga(title):
-    pass
+    panels = glob.glob(f"dataset/{title}/*.jpg")
+    
+    images = [transform(Image.open(panel_file)) for panel_file in panels]
+
+    
+    manga_tensor = torch.stack(images)
+    
+    # Run through the models, transform from (50, 3, 224, 224) to (50, 1280)
+    panel_vectors = model(manga_tensor)
+    
+    # Really need to dive back in what is shape and tensor dimensions ???
+
+    # Average out the vector for each mangas (50 panels each), shape = (1280,)
+    avg_panel_vector = torch.mean(panel_vectors, dim=0)
+
+    return avg_panel_vector
